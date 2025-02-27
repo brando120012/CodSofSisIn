470725b877f86d1de71d24ca6cac6884b35e630b
brando120013 <ramirez.brandon.2024@cbtis-246.edu.mx>
2025-02-27 11:46:36 -0600

    practica 4 juan antonio.py
    practica 4 juan antonio.py

---
+++ practica 4  juan antonio.py
@@ -0,0 +1,13 @@
+#Pregunta al usuario por sus comida favorita y escribe el numero de letras que contiene su comida.
+
+#Codificar una solución que escriba el siguiente texto llenando los espacios  en blanco con valiables que tu definas:
+
+##"Mi nombre es y tengo  años de edad. También, Siempre he querido ser  . Gracias por leer me!"
+
+nombre=input("Mi nombre es")
+edad=input("y tengo")
+profesion=input("Siempre he querido ser")
+comida=input("Mi comida favorita es")
+pais=input("Mi pais favorito es")
+
+print("Mi nombre es ",nombre, "y tengo ", edad, " años de edad. Tambien, siempre he querido ser", profesion, "Mi comida favorita es",comida, "Mi pais favorito es",pais,  ".")
